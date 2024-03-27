b413bfb44e6bc579c1e1cde6e146689802cc153f
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-03-27 14:37:14 +0100

    labels and columns

--- app/command-editor.tsx
+++ app/command-editor.tsx
@@ -5,6 +5,7 @@ import { ApplicationCommandType, useCommandStore } from "./commands";
 import { Input } from "@/components/input";
 import { Select } from "@/components/select";
 import { Stack } from "@/components/stack";
+import { Label } from "@/components/label";
 
 export function CommandEditor() {
   const commands = useCommandStore((state) => state.commands);
@@ -18,12 +19,15 @@ export function CommandEditor() {
       <Stack size="large">
         {commands.map((command: any, i: number) => (
           <Stack key={i} size="small">
-            <Input value={command.name} onChange={(e) => renameCommand(i, e.currentTarget.value)} />
+            <Label htmlFor="name">Name</Label>
+            <Input name="name" value={command.name} onChange={(e) => renameCommand(i, e.currentTarget.value)} />
+            <Label htmlFor="name">Type</Label>
             <Select value={command.type} onChange={(e) => changeCommandType(i, parseInt(e.currentTarget.value))}>
               <option value={ApplicationCommandType.CHAT_INPUT}>Chat</option>
               <option value={ApplicationCommandType.USER}>User</option>
               <option value={ApplicationCommandType.MESSAGE}>Message</option>
             </Select>
+            <Label htmlFor="name">Description</Label>
             <Input value={command.description} onChange={(e) => changeCommandDescription(i, e.currentTarget.value)} />
           </Stack>
         ))}
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ import { CommandsJSONPreview } from "./json-output";
 
 export default function Home() {
   return (
-    <main className="flex flex-auto">
-      <CommandEditor />
-      <CommandsJSONPreview />
+    <main className="flex flex-auto flex-wrap">
+      <div className="flex-1" style={{ minWidth: "60ch" }}>
+        <CommandEditor />
+      </div>
+      <div className="flex-1" style={{ minWidth: "60ch" }}>
+        <CommandsJSONPreview />
+      </div>
     </main>
   );
 }
