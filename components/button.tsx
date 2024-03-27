7aedfbfff37f377b3f0f3412c8a407598ddbbf55
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-03-27 14:58:44 +0100

    single command and styling

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
 
 export function Button(props: React.ComponentProps<"button">) {
   return (
-    <button {...props} className="bg-indigo-500 text-slate-100 px-4 py-1 rounded hover:bg-indigo-600 transition-all duration-200">
+    <button {...props} className="bg-indigo-500 text-zinc-100 px-4 py-1 rounded hover:bg-indigo-600 transition-all duration-200">
       {props.children}
     </button>
   )
